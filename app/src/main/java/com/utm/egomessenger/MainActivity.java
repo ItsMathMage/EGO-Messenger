2fc91781ada406e512c088e4d23e81392637f484
Yulian <yuliantsvetkovskiy@gmail.com>
2021-02-04 14:39:34 +0300

    Добавив два класа User i Message,також в дизайні action-bar i background

--- app/src/main/java/com/utm/egomessenger/MainActivity.java
+++ app/src/main/java/com/utm/egomessenger/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    private User user;
+
+    //При запуску програми
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
---
+++ app/src/main/java/com/utm/egomessenger/User.java
@@ -0,0 +1,49 @@
+package com.utm.egomessenger;
+
+public class User {
+    private String email;
+    private String initials;
+    private String phone;
+    private String password;
+
+    public User() {}
+
+    public User(String email, String initials, String phone, String password) {
+        this.email = email;
+        this.initials = initials;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getInitials() {
+        return initials;
+    }
+
+    public void setInitials(String initials) {
+        this.initials = initials;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
